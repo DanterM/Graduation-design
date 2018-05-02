1ed1cf94ac29328385a527d57d3a4be864234e04
DanterM <jarvis4m@gmail.com>
2018-05-02 21:10:45 +0800

    准确率20%

--- 任务/2018-05-02基于随机森林对energydata进行能耗预测/3.py
+++ 任务/2018-05-02基于随机森林对energydata进行能耗预测/3.py
@@ -40,7 +40,7 @@ print(train_data)
 
 # 画图
 plt.figure(figsize=(12,8)).show()
-sns.countplot(x='label', data=train_df)
+sns.countplot(x='Appliances', data=train_df)
 plt.title('Distribution of Numbers')
 plt.xlabel('Numbers');
 plt.show()
@@ -48,16 +48,16 @@ plt.show()
 # 2/3的train_data作为训练数据，1/3的train_data作为测试数据来训练模型
 num_features = train_data.shape[0] # 拿到train_data的行数，也就是所有数据的个数作为特征值
 print("Number of all features: \t\t", num_features)
-split = int(num_features * 2/3) # 这里是取2/3行也就是前28000行作为训练 后1/3也就是14000作为测试
+split = int(num_features * 2/3) # 这里是取2/3行作为训练 后1/3行作为测试
 
-train = train_data[:split] # 取出前28000行作为训练数据
-test = train_data[split:] # 取出后14000行作为测试数据
+train = train_data[:split] # 取出前2/3行作为训练数据
+test = train_data[split:] # 取出后1/3行作为测试数据
 
 print("Number of features used for training: \t", len(train),
       "\nNumber of features used for testing: \t", len(test))
 
 # 开始使用随机森林分类器
-clf = RandomForestClassifier(n_estimators=100) # 定义决策树的个数为100
+clf = RandomForestClassifier(n_estimators=200) # 定义决策树的个数为100
 
 # 开始训练，训练的X数据格式为[[]]，训练的y值为[]也就是经过ravel后的数据
 # 如果你问我ravel()的作用是什么，就是不管什么数据格式的数据都转成一个一维的array，这样每个元素都是一个平等且顺序的位置
@@ -77,6 +77,8 @@ clf = RandomForestClassifier(n_estimators=100) # 100 trees
 target = train_data[:,0].ravel()
 train = train_data[:,1:]
 model = clf.fit(train, target)
+
+
 
 # 用测试集数据来预测最终结果
 output = model.predict(test_data)
