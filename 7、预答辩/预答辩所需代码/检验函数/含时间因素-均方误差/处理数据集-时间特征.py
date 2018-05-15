d2c09f38cdced259aeac2aff0f78143131b5eaa6
DanterM <jarvis4m@gmail.com>
2018-05-16 01:33:55 +0800

    增加energydata可视化

---
+++ 7、预答辩/预答辩所需代码/检验函数/含时间因素-均方误差/处理数据集-时间特征.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+train_df = pd.read_csv('../dataset/1energydata-del1day.csv')
+
+train_data = train_df['date']
+print(train_data)
+
+# for i in train_data:
+#
+#     print(train_data[[i]])
+
+# print(train_df['date'][0])
+
+# if train_df['date'][0] = 1:
+#     print()
+
+for i in train_df['date']:
+    if (train_df['date'][i] == '0:00'):
+        print('success')
+
+
+def numbers_to_strings(argument):
+    switcher = {
+        0: "zero",
+        1: "one",
+        2: "two",
+    }
+    return switcher.get(argument, "nothing")
